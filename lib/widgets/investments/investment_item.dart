24a370ab5ef65cbd6422c2acd7f61e4cbee394c3
lohnn <johannes@lohnn.se>
2024-09-12 11:08:42 +0200

    You can now check information about your investments

--- lib/widgets/investments/investment_item.dart
+++ lib/widgets/investments/investment_item.dart
@@ -24,6 +24,69 @@ class InvestmentItem extends StatelessWidget {
           ),
         ),
         ListTile(
+          onTap: () {
+            showModalBottomSheet(
+              useSafeArea: true,
+              context: context,
+              builder: (context) {
+                return ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.asset(
+                          'assets/${investment.investmentType.name}.webp',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(32.0),
+                        color: Colors.white.withOpacity(0.8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              getInvestmentTypeName(investment.investmentType),
+                              style: const TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                shadows: [],
+                              ),
+                            ),
+                            Text(
+                              getInvestmentDescription(investment.investmentType),
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              'Antal: ${investment.quantity}',
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              'Pris: ${investment.value.toInt()}',
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
           leading: Text(
             getInvestmentTypeName(investment.investmentType),
             textAlign: TextAlign.left,
